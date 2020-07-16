320df6e0bef15fd719ccfbd35b7c99ccf40433ea
Márk Mosonyi <mosonyi.mark95@gmail.com>
2020-07-16 15:13:08 +0200

    Finish with Hard one task

--- week-01/Day-4/src/loops/DrawChessTable.java
+++ week-01/Day-4/src/loops/DrawChessTable.java
@@ -1,4 +1,31 @@
 package loops;
 
+
+import java.util.Scanner;
+
 public class DrawChessTable {
+  // Crate a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Give me the number of lines ");
+    int lines = input.nextInt();
+
+    for (int i = 1; i <= lines; i++) {
+      if (i % 2 == 0) {
+        System.out.print("  ");
+      }
+      System.out.print("% % % %");
+      System.out.println("");
+    }
+  }
 }
--- week-01/Day-4/src/loops/ParametricAverage.java
+++ week-01/Day-4/src/loops/ParametricAverage.java
@@ -1,17 +1,27 @@
 package loops;
 
+
 import java.util.Scanner;
 
 public class ParametricAverage {
-  // Write a program that asks for a number.
-// It would ask this many times to enter an integer,
-// if all the integers are entered, it should print the sum and average of these
-// integers like:
-//
-// Sum: 22, Average: 4.4
-
   public static void main(String[] args) {
+    // Write a program that asks for a number.
+    // It would ask this many times to enter an integer,
+    // if all the integers are entered, it should print the sum and average of these
+    // integers like:
+    //
+    // Sum: 22, Average: 4.4
     Scanner sc = new Scanner(System.in);
-    
+    System.out.println("How many integers do you want?");
+    int givenIntegers = sc.nextInt();
+    int sum = 0;
+
+    for (int i = 0; i < givenIntegers; i++) {
+      System.out.println("Give me next number: ");
+      int number = sc.nextInt();
+      sum += number;
+    }
+    double average = (double) sum / givenIntegers;
+    System.out.println("SUM: " + sum + " , " + "Avg: " + average);
   }
 }
